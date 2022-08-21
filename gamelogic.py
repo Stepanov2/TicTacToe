a2c7db8999dcde612448fbbe0d9e59704b0c7b61
Dmitry Stepanov <bugagolick@gmail.com>
2022-08-21 19:27:38 +0300

    Потому что две чёртовых лишних скобки=( grid_line.append(0)

--- gamelogic.py
+++ gamelogic.py
@@ -14,7 +14,7 @@ def initialize_playfield():
     for i in range(grid_size):
         grid_line=[]
         for j in range(grid_size):
-            grid_line.append([0])
+            grid_line.append(0) #aaaa
         playfield.append(grid_line)
     return
 
@@ -30,17 +30,26 @@ def check_win_condition(one_or_minus_one):
     global grid_size
     global in_line
     won = one_or_minus_one * in_line
+
     max_offset = grid_size - in_line
-    #checking rows
-    for i in range(0, grid_size):
-        for j in range(0, max_offset + 1):
-            print(playfield[i][j:j + in_line])
-            if sum(playfield[i][j:j + in_line]) == won:
-                #ПОЧЕМУ ЭТО НЕ РАБОТАЕТ?11
+    #checking rows and columns
+    for offset in range(max_offset + 1):
+
+        for i in range(in_line):
+            row_sum, col_sum = won, won
+
+            for j in range(in_line):
+                print(playfield[i][j+offset])
+                row_sum-=playfield[i][j+offset]
+                col_sum-=playfield[j+offset][i]
+            if not row_sum or not col_sum:
                 return True
 
-    return
-initialize_playfield()
-#print(playfield[0][0:3])
 
-check_win_condition(1)
+
+
+    return False
+initialize_playfield()
+print(playfield)
+
+print(check_win_condition(1))
